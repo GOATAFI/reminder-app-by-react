f9c04c0151634c7c8433fd48bb284e8b7f5a1448
GOATAFI <projectexecutiveofpm@gmail.com>
2024-07-31 23:58:11 +0600

    lifting the state up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import FilterSelect from "./FilterSelect";
 import InputForm from "./InputForm";
 import ReminderList from "./ReminderList";
 
 function App() {
+  const [reminders, setReminders] = useState();
+  const [userInput, setUserInput] = useState();
+  const [selectedFilter, setSelectedFilter] = useState("all");
   return (
     <div>
-      <InputForm />
-      <FilterSelect />
-      <ReminderList />
+      <InputForm userInput={userInput} setUserInput={setUserInput} />
+      <FilterSelect
+        selectedFilter={selectedFilter}
+        setSelectedFilter={setSelectedFilter}
+      />
+      <ReminderList reminders={reminders} />
     </div>
   );
 }
