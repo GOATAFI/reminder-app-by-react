537b42707627796c69112acef0ddf41efbe1ac3b
GOATAFI <projectexecutiveofpm@gmail.com>
2024-08-01 19:42:15 +0600

    app thke props akare data send kora done

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import InputForm from "./InputForm";
 import ReminderList from "./ReminderList";
 
 function App() {
-  const [reminders, setReminders] = useState();
+  const [reminders, setReminders] = useState([
+    {
+      reminderText: "No reminders",
+      dueDate: new Date().toISOString().substr(0, 10),
+      isComplete: false,
+    },
+  ]);
   const [userInput, setUserInput] = useState();
   const [selectedFilter, setSelectedFilter] = useState("all");
   return (
